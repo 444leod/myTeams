3b38864fe058384c1bd5c93dfd8bc9f9ad62def8
Léo DUMONT <leo.dumont@epitech.eu>
2024-04-11 14:11:40 +0200

    feat(magic number): added magic number verification for the first 4 bytes of the file.

---
+++ server/include/magic_number.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** myTeams
+** File description:
+** magic_number
+*/
+
+#pragma once
+
+#include <stdbool.h>
+
+#define MAGIC_NUMBER 0xdeadbeef
+
+bool is_magic_number_valid(int fd);
---
+++ server/src/lib/magic_number.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** myTeams
+** File description:
+** magic_number
+*/
+
+#include "magic_number.h"
+#include <stdbool.h>
+#include <unistd.h>
+
+bool is_magic_number_valid(int fd)
+{
+    unsigned int magic_number = 0;
+
+    if (read(fd, &magic_number, sizeof(magic_number)) == -1)
+        return false;
+    return (magic_number == MAGIC_NUMBER);
+}
