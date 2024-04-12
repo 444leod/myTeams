32a4c41a76be184ad9a59397f55ed2b9ff63fdbd
Léo DUMONT <leo.dumont@epitech.eu>
2024-04-12 15:44:54 +0200

    feat(logout command): added logout command handling.

--- server/src/commands/logout.c
+++ server/src/commands/logout.c
@@ -6,9 +6,42 @@
 */
 
 #include "commands.h"
+#include "reply_code.h"
+#include "logging_server.h"
+#include "packet.h"
+
+/**
+ * @brief Check if the command is valid
+ * @details Check if the command is valid
+ *
+ * @param client the client
+ * @param command the command
+ *
+ * @return true if the command is valid
+ * @return false if the command is not valid
+ */
+static bool is_command_valid(client_t client, char **command)
+{
+    if (tablen((void **)command) != 1) {
+        client->packet = build_packet(SYNTAX_ERROR_IN_PARAMETERS, "");
+        return false;
+    }
+    if (!client->user) {
+        client->packet = build_packet(NOT_LOGGED_IN, "");
+        return false;
+    }
+    return true;
+}
 
 void logout(client_t client, char **command)
 {
-    (void)client;
-    (void)command;
+    user_t user = client->user;
+
+    if (!is_command_valid(client, command))
+        return;
+    client->packet =
+        build_userinfo_packet(USER_LOGGED_OUT, user->username, user->uuid);
+    user->status = STATUS_NOT_LOGGED_IN;
+    client->user = NULL;
+    server_event_user_logged_out(get_uuid_as_string(user->uuid));
 }
