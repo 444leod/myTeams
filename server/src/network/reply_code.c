57a1e6d46121e5bb27aff11ef155bf35824f3bd2
Léo DUMONT <leo.dumont@epitech.eu>
2024-04-12 02:00:37 +0200

    update(server packet sending): updated the way the server send packet to client, using packets.

--- server/src/network/reply_code.c
+++ server/src/network/reply_code.c
@@ -13,7 +13,16 @@
 #include "lib.h"
 #include "garbage_collector.h"
 
-const enum REPLY_CODES custom_messages_list[] = {
+/**
+ * @brief The custom messages list
+ * @details The list of custom messages that the server can send to the client
+ * it uses the client buffer to send the message
+*/
+const int custom_messages_list[] = {
+    HELP_MESSAGE,
+    USER_CREATED,
+    USER_LOGGED_IN,
+    USER_LOGGED_OUT,
     USERS_LIST,
     USER_INFO,
     TEAM_LIST,
@@ -24,6 +33,7 @@ const enum REPLY_CODES custom_messages_list[] = {
     TEAM_INFO,
     CHANNEL_INFO,
     THREAD_INFO,
+    -1
 };
 
 /**
@@ -32,53 +42,52 @@ const enum REPLY_CODES custom_messages_list[] = {
  * current code
 */
 const server_message_t serverMessages[] = {
-    {COMMAND_OK, "%d Command okay.\n"},
-    {HELP_MESSAGE, "%d-%s\n%d Help message.\n"},
-    {GLOBAL_CONTEXT_SET, "%d Global context set.\n"},
-    {TEAM_CONTEXT_SET, "%d Team context set.\n"},
-    {CHANNEL_CONTEXT_SET, "%d Channel context set.\n"},
-    {THREAD_CONTEXT_SET, "%d Thread context set.\n"},
-    {USER_CREATED,
-        "%d User created (username: \"%s\", uuid: \"%s\"). User logged in.\n"},
-    {USER_LOGGED_IN, "%d User logged in, proceed.\n"},
-    {USER_LOGGED_OUT, "%d User logged out.\n"},
-    {SERVICE_READY_NEW_USER, "%d Service ready for new user.\n"},
-    {MESSAGE_SENT, "%d Message sent.\n"},
-    {TEAM_SUBSCRIBED, "%d Team subscribed.\n"},
-    {TEAM_UNSUBSCRIBED, "%d Team unsubscribed.\n"},
-    {TEAM_IS_SUBSCRIBED, "%d Current user is subscribed.\n"},
-    {TEAM_IS_NOT_SUBSCRIBED, "%d Current user is not subscribed.\n"},
-    {TEAM_CREATED, "%d Team created.\n"},
-    {CHANNEL_CREATED, "%d Channel created.\n"},
-    {THREAD_CREATED, "%d Thread created.\n"},
-    {REPLY_CREATED, "%d Reply created.\n"},
+    {COMMAND_OK, "Command okay."},
 
-    {SYNTAX_ERROR, "%d Syntax error, command unrecognized.\n"},
+    {GLOBAL_CONTEXT_SET, "Global context set."},
+    {TEAM_CONTEXT_SET, "Team context set."},
+    {CHANNEL_CONTEXT_SET, "Channel context set."},
+    {THREAD_CONTEXT_SET, "Thread context set."},
+
+    {SERVICE_READY_NEW_USER, "Service ready for new user."},
+
+    {MESSAGE_SENT, "Message sent."},
+
+    {TEAM_SUBSCRIBED, "Team subscribed."},
+    {TEAM_UNSUBSCRIBED, "Team unsubscribed."},
+    {TEAM_IS_SUBSCRIBED, "Current user is subscribed."},
+    {TEAM_IS_NOT_SUBSCRIBED, "Current user is not subscribed."},
+    {TEAM_CREATED, "Team created."},
+    {CHANNEL_CREATED, "Channel created."},
+    {THREAD_CREATED, "Thread created."},
+    {REPLY_CREATED, "Reply created."},
+
+    {SYNTAX_ERROR, "Syntax error, command unrecognized."},
     {SYNTAX_ERROR_IN_PARAMETERS,
-        "%d Syntax error in parameters or arguments.\n"},
-    {COMMAND_NOT_IMPLEMENTED, "%d Command not implemented.\n"},
-    {BAD_COMMAND_SEQUENCE, "%d Bad sequence of commands.\n"},
+        "Syntax error in parameters or arguments."},
+    {COMMAND_NOT_IMPLEMENTED, "Command not implemented."},
+    {BAD_COMMAND_SEQUENCE, "Bad sequence of commands."},
     {COMMAND_NOT_IMPLEMENTED_FOR_PARAMETER,
-        "%d Command not implemented for that parameter.\n"},
-    {NAME_TOO_LONG, "%d Name too long.\n"},
-    {DESCRIPTION_TOO_LONG, "%d Description too long.\n"},
-    {BODY_TOO_LONG, "%d Body too long.\n"},
-    {INEXISTANT_TEAM, "%d Inexistant team.\n"},
-    {INEXISTANT_CHANNEL, "%d Inexistant channel.\n"},
-    {INEXISTANT_THREAD, "%d Inexistant thread.\n"},
-    {INEXISTANT_USER, "%d Inexistant user.\n"},
-    {ALREADY_SUBSCRIBED, "%d Already subscribed.\n"},
-    {NOT_SUBSCRIBED, "%d Not subscribed.\n"},
-    {ALREADY_LOGGED_IN, "%d Already logged in.\n"},
-    {ALREADY_LOGGED_OUT, "%d Already logged out.\n"},
-    {ALREADY_EXISTS, "%d Already exists.\n"},
+        "Command not implemented for that parameter."},
+    {NAME_TOO_LONG, "Name too long."},
+    {DESCRIPTION_TOO_LONG, "Description too long."},
+    {BODY_TOO_LONG, "Body too long."},
+    {INEXISTANT_TEAM, "Inexistant team."},
+    {INEXISTANT_CHANNEL, "Inexistant channel."},
+    {INEXISTANT_THREAD, "Inexistant thread."},
+    {INEXISTANT_USER, "Inexistant user."},
+    {ALREADY_SUBSCRIBED, "Already subscribed."},
+    {NOT_SUBSCRIBED, "Not subscribed."},
+    {ALREADY_LOGGED_IN, "Already logged in."},
+    {ALREADY_LOGGED_OUT, "Already logged out."},
+    {ALREADY_EXISTS, "Already exists."},
     {UNSUFFICIENT_PERMISSIONS,
-        "%d Unsufficient permissions to perform this action.\n"},
+        "Unsufficient permissions to perform this action."},
     {FORBIDDEN_CHARACTERS,
-        "%d Forbidden characters used, only alphanumerical "
-        "characters are allowed.\n"},
-    {NOT_LOGGED_IN, "%d Not logged in.\n"},
-    {-1, (void *)0}
+        "Forbidden characters used, only alphanumerical "
+        "characters are allowed."},
+    {NOT_LOGGED_IN, "Not logged in."},
+    {-1, "Something went wrong with the server."}
 };
 
 /**
@@ -90,27 +99,33 @@ const server_message_t serverMessages[] = {
  *
  * @return true if the current code is a special reply code
 */
-static bool special_reply_code(client_t client)
+static bool special_reply_code(int code)
 {
-    int socketFd = client->fd;
-    int code = client->current_code;
-
-    switch (code) {
-        case USER_LOGGED_OUT:
-            dprintf(socketFd, serverMessages[6].message, code);
-            remove_client(client->fd);
-            return true;
-        case HELP_MESSAGE:
-            dprintf(socketFd, serverMessages[4].message, code,
-                client->buffer, code);
-            return true;
-        case USER_CREATED:
-            dprintf(socketFd, serverMessages[6].message, code,
-                client->user->username,
-                get_uuid_as_string(client->user->uuid));
+    for (uint16_t i = 0; custom_messages_list[i] != -1; i++) {
+        if (code == custom_messages_list[i])
             return true;
     }
     return false;
+}
+
+void rebuild_packet(int code, client_t client)
+{
+    packet_t *packet = client->packet;
+    uint16_t i = 0;
+
+    if (special_reply_code(code))
+        return;
+    for (i = 0; serverMessages[i].code != -1; i++) {
+        if (serverMessages[i].code == code) {
+            my_free(packet);
+            packet = build_packet(code, my_strdup(serverMessages[i].message));
+            client->packet = packet;
+            return;
+        }
+    }
+    my_free(packet);
+    packet = build_packet(UNKNOWN_ERROR, my_strdup(serverMessages[i].message));
+    client->packet = packet;
 }
 
 /**
@@ -122,15 +137,9 @@ static bool special_reply_code(client_t client)
 void reply_code(client_t client)
 {
     int socketFd = client->fd;
-    int code = client->current_code;
+    int code = client->packet->code;
 
-    if (special_reply_code(client))
-        return;
-    for (int i = 0; serverMessages[i].code != -1; i++) {
-        if (serverMessages[i].code == code) {
-            dprintf(socketFd, serverMessages[i].message, code);
-            return;
-        }
-    }
-    dprintf(socketFd, "%d Unknown reply code.\n", code);
+    rebuild_packet(code, client);
+    send_packet(socketFd, client->packet);
+    my_free(client->packet);
 }
